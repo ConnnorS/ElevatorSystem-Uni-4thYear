0f1fbf584ed9285583196d480742dc2112e4adbe
ConnnorS <connorsouth117@protonmail.com>
2024-09-20 13:45:22 +1000

    Added handling for args

--- internal.c
+++ internal.c
@@ -1,3 +1,29 @@
+// basic C stuff
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// threads
+#include <pthread.h>
+// shared memory
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void verify_operation(const char operation[8])
+{
+  const char operations[5][8] = {"Opening", "Open", "Closed", "Closing", "Between"};
+  for (int i = 0; i < 5; i++)
+  {
+    if (strcmp(operation, operations[i]) == 0)
+    {
+      return;
+    }
+  }
+
+  printf("Invalid operation specified\nPlease enter \"Opening\", \"Open\", \"Closed\", \"Closing\", or \"Between\"\n");
+  exit(1);
+}
+
 /* expectes {car name} {operation}*/
 int main(int argc, char **argv)
 {
@@ -6,5 +32,13 @@ int main(int argc, char **argv)
     printf("Useage %s {car name} {operation}\n", argv[0]);
     exit(1);
   }
+
+  const char car_name[32];
+  strcpy((char *)car_name, argv[1]);
+  const char car_operation[8];
+  strcpy((char *)car_operation, argv[2]);
+
+  verify_operation((char *)car_operation);
+
   return 0;
 }
