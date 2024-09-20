3bf16e8c8ab96279d3fe56aa9eccc83b9ec89e91
ConnnorS <connorsouth117@protonmail.com>
2024-09-20 10:59:18 +1000

    Added in handling for internal.c args

--- internal.c
+++ internal.c
@@ -1,5 +1,10 @@
 /* expectes {car name} {operation}*/
 int main(int argc, char **argv)
 {
+  if (argc != 3)
+  {
+    printf("Useage %s {car name} {operation}\n", argv[0]);
+    exit(1);
+  }
   return 0;
 }
