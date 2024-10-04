43e894546dbb8a32b64fe1dfe1dd81e373388e45
ConnnorS <connorsouth117@protonmail.com>
2024-10-04 10:56:36 +1000

    Reset

--- Car/car_status_operations.h
+++
@@ -1,8 +0,0 @@
-#include "car_helpers.h"
-
-void opening_doors(connect_data_t *data);
-void open_doors(connect_data_t *data);
-void closing_doors(connect_data_t *data);
-void close_doors(connect_data_t *data);
-void set_between(connect_data_t *data);
-void get_status(connect_data_t *data, char *status);
